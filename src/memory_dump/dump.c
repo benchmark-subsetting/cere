83bc8380bdbedfa93f4b80ac1fa20db4ddbdf468
Chadi Akel <chadi.akel@exascale-computing.eu>
2013-10-15 11:10:45 +0200

    *The name of the loop to dump is given to the dump lib.

--- src/memory_dump/dump.c
+++ src/memory_dump/dump.c
@@ -8,20 +8,26 @@
 #include <sys/types.h> 
 #include <unistd.h>
 #include <sys/mman.h>
+#include <string.h>
 
- 
+const int MAX_DUMPABLE_LOOPS = 250;
+const int MAX_LOOP_NAME = 64;
+int NUMBER_OF_LOOPS_DUMPED = 0;
+char LOOPS_DUMPED[MAX_DUMPABLE_LOOPS][MAX_LOOP_NAME];
+
 /* dump_region: dumps to a file "<start>.memdump"
  * the memory segment starting at address <start>
  * and ending at address <end>
  */
-void dump_region(int mem, 
+void dump_region(char* loop_name,
+                 int mem, 
                  off64_t start, 
                  off64_t end)
 {
     char buf[4096];
     char path[256];
 
-    snprintf(path, sizeof(path), "%lx.memdump", start);
+    snprintf(path, sizeof(path), "%s.%lx.memdump", loop_name, start);
     int out = open(path, O_WRONLY|O_CREAT,S_IRWXU);
 
     if (out == -1) {
@@ -46,18 +52,19 @@ void dump_region(int mem,
  * count: the number of addresses
  * addresses: the addresses that we want to preserve
  */
-void dump_mem(pid_t child, int count, void* addresses[]) {
+void dump_mem(char* loop_name, pid_t child, int count, void* addresses[]) {
     /* Wait for the child to stop */
     wait(NULL);
 
     FILE *maps;
     int mem;
     int i;
-    char path[256];
+    char path[256], *core_name;
 
-    FILE * core_map = fopen("core.map", "w"); 
+    sprintf(core_name, "%s.core.map", loop_name);
+    FILE * core_map = fopen(core_name, "w"); 
         if (!core_map) {
-        fprintf(stderr, "Could not create core.map file");
+        fprintf(stderr, "Could not create %s file", core_name);
         exit(-1);
     }
 
@@ -93,7 +100,7 @@ void dump_mem(pid_t child, int count, void* addresses[]) {
         if (start < 0x500000
             || start > 0xffffffffff000000) continue;
         /* dump the region */
-        dump_region(mem, start, end);
+        dump_region(loop_name, mem, start, end);
     }
 
     close(mem);
@@ -102,9 +109,17 @@ void dump_mem(pid_t child, int count, void* addresses[]) {
  
 /* dump: dumps memory
  * count: number of arguments
+ * loop_name: name of dumped loop
  * args: the arguments to dump 
  */
-void dump(int count, ...) {
+void dump(char* loop_name, int count, ...) {
+    int i;
+    for (i = 0; i < NUMBER_OF_LOOPS_DUMPED; i++)
+    {
+        if(!strcmp(loop_name, LOOPS_DUMPED[i])) return;
+    }
+    strcpy(LOOPS_DUMPED[NUMBER_OF_LOOPS_DUMPED], loop_name);
+    NUMBER_OF_LOOPS_DUMPED++;
     void * addresses[count];
     va_list ap;
     int j;
@@ -121,7 +136,7 @@ void dump(int count, ...) {
         raise(SIGTRAP);
     }
     else {
-        dump_mem(child, count, addresses);
+        dump_mem(loop_name, child, count, addresses);
         ptrace(PTRACE_CONT, child, NULL, NULL);
         exit(0);
     }
@@ -135,10 +150,12 @@ void dump(int count, ...) {
  * count: number of args
  * addresses: an array of <count> addresses
  */
-void load(int count, void* addresses[]) {
-    FILE* core_map = fopen("core.map", "r");
+void load(int count, char* loop_name, void* addresses[]) {
+    char *core_name;
+    sprintf(core_name, "%s.core.map", loop_name);
+    FILE* core_map = fopen(core_name, "r");
     if (!core_map) {
-        fprintf(stderr, "Could not open core.map");
+        fprintf(stderr, "Could not open %s", core_name);
         exit(-1);
     }
     
