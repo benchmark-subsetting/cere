22e0b90a0652f3398c04e66ab73e7c5c453932ef
Pablo de Oliveira <pablo@sifflez.org>
2013-10-22 16:02:27 +0200

    Fix Makefile so that the segments are linked in replay mode

--- tests/fake_test/test.c
+++ tests/fake_test/test.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int checksum(int *a, int size)
 {
     int check=0, i;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size; i++) {
+        if (i == 0) {
+            fprintf(stderr, "check: %d, &a: %x, a[0]: %d\n", check, a, a[0]);
+        }
         check += a[i];
+        if (i == size-1)
+            fprintf(stderr, "Checksum invitro: %d\n", check);
+    }
     return check;
 }
 
@@ -18,7 +25,7 @@ void init(int *a, int size)
 int main(int argc, char **argv)
 {
     int size=512;
-    int a[size];
+    int * a = malloc(sizeof(int)*size);
     init(a, size);
     int check = checksum(a, size);
     fprintf(stderr, "Checksum : %d\n", check);
