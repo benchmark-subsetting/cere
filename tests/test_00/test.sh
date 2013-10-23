b5ec0eb082fa3bd955654e2888f8254fc19cce26
Pablo de Oliveira <pablo@sifflez.org>
2013-10-23 13:46:37 +0200

    Make test_00 silent

--- tests/test_00/test.sh
+++ tests/test_00/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-make veryclean && make test_dump 
+make veryclean > /dev/null 2>&1 && make test_dump  > /dev/null 2>&1
 ./test_dump 2> /tmp/test.dump.out
-make test_replay
+make test_replay > /dev/null 2>&1
 ./test_replay 2> /tmp/test.replay.out
 
 cat /tmp/test.dump.out | grep "&a" | head -n1 > /tmp/test.a
