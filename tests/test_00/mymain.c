a74c7386849122e9d8a88a4c8aa5e5feec481201
Chadi Akel <chadi.akel@exascale-computing.eu>
2013-10-29 11:27:35 +0100

    Added the static patcher to the linking chain in lel script.
    Fixed tests 00, 01.
    Test 02 is not working because of the fork problem in the load
    lib that makes strange behavior with IS code.

--- tests/test_00/mymain.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-void run__extracted__checksum();
-
-void real_main()
-{
-    run__extracted__test_checksum_7_7();
-    exit(EXIT_SUCCESS);
-}
--- tests/test_02/test.sh
+++ tests/test_02/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-./IS > /dev/null 2> ./out 
 
-grep "rank_for.cond" out > /dev/null
+make veryclean > /dev/null 2>&1
+make MODE=--dump > /dev/null 2>&1
+./IS > /dev/null 2>&1
+
+make clean > /dev/null 2>&1
+make MODE=--replay=__extracted__is_main_724_724 > /dev/null 2>&1
+./IS > /tmp/test.replay.out
+
+cat /tmp/test.replay.out | head -10 > /tmp/test.a
+
+diff /tmp/test.a verif
 exit $?
