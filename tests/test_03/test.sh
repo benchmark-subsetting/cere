25b28acb0b6aa11849b4286f4322f8911de84aee
Chadi Akel <chadi.akel@exascale-computing.eu>
2013-11-07 09:58:23 +0100

    Instrumentation works for in-vivo or in-vitro loops. To instrument loops, pass to lel and lec the --instrument option.

--- tests/test_03/test.sh
+++ tests/test_03/test.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
-make veryclean > /dev/null 2>&1 && make test_dump  > /dev/null 2>&1
-./test_dump 2> /tmp/test.dump.out
+#!/bin/bash
+make veryclean > /dev/null 2>&1
+make MODE=--dump  > /dev/null 2>&1
+./BT > /dev/null 2>&1
 
-make test_replay > /dev/null 2>&1
-./test_replay 2> /tmp/test.replay.out
+make clean > /dev/null 2>&1
+make MODE=--replay=__extracted__verify_verify__265 INSTRU=--instrument > /dev/null 2>&1
+./BT > /tmp/test.replay.out
 
-cat /tmp/test.dump.out | grep "&a" | head -n1 > /tmp/test.a
-cat /tmp/test.replay.out | grep "&a" | head -n1 > /tmp/test.b
-
-diff /tmp/test.a /tmp/test.b
-exit $?
+if [ -f rdtsc_result.csv ]; then
+    exit 0
+else
+    exit 1
+fi
+#exit $?
