bf9c44263b7cbd485fca45f7918adec1baec7589
Pablo de Oliveira <pablo@sifflez.org>
2013-11-28 19:19:51 +0100

    Add granularity choser.
    This program solves an ILP problem to select a set of codelets that
    satisfy:
    
     * minimal cost when benchmarking
    
    under the constraint of:
     * coverage higher than THRESHOLD
     * individual running time higher than THRESHOLD
     * never select at the same time a codelet and its parent

---
+++ src/granularity/granularity.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import csv
+import argparse
+from pulp import LpInteger, LpMinimize, LpProblem, LpVariable
+
+class Codelet:
+    def __init__(self, name, callcount, totalcycles):
+        self.name = name
+        self.callcount = callcount
+        # we store cycles per iteration
+        self.cycles = float(totalcycles)/callcount
+
+    def __repr__(self):
+        return self.name
+
+    def is_children_of(self, other):
+        return self.name != other.name and self.name.startswith(other.name)
+
+def parse(csvfile):
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader.next() # skip header
+    for row in csvreader:
+        yield Codelet(row[0], int(row[1]), int(row[2]))
+    csvfile.close()
+
+def solve(codelets, appli_cycles, min_cycles=10**6, min_coverage=0.8):
+    prob = LpProblem("granularity selection", LpMinimize)
+    codelet_vars = LpVariable.dicts("codelet", 
+                                    codelets, 
+                                    lowBound=0, 
+                                    upBound=1, 
+                                    cat=LpInteger) 
+
+    # Objective function: the codelet total cost must be minimal
+    # we want small codelets
+    prob += sum([codelet_vars[c]*c.cycles for c in codelets])
+
+    # But not too small, we require at least threshold cycles for chosen 
+    # codelets
+    for c in codelets:
+        prob += c.cycles*c.callcount >= min_cycles*codelet_vars[c]
+
+    # and with good coverage
+    prob += (sum([codelet_vars[c]*c.cycles*c.callcount for c in codelets]) 
+                >= min_coverage * appli_cycles)
+
+    # Finally we should never include both the children and the parents
+    for dad in codelets: 
+        for son in codelets:
+            # XXX this can be implemented much more efficiently,
+            # by using the transitivity of being a parent
+            if not son.is_children_of(dad): continue
+
+            # We cannot select dad and son at the same time 
+            prob += codelet_vars[dad] + codelet_vars[son] <= 1
+
+    prob.solve()
+    for v in prob.variables():
+        if v.varValue == 1.0:
+            for c in codelets:
+                if "codelet_"+c.name == v.name: 
+                    yield c
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find optimal codelet granularity")
+    parser.add_argument('profile_file', type=file) 
+    parser.add_argument('application_cycles', type=int)
+    parser.add_argument('--min_cycles', type=int, default=10**6)
+    parser.add_argument('--min_coverage', type=float, default=0.8)
+
+    args = parser.parse_args()
+
+    codelets = list(parse(args.profile_file))
+    chosen = list(solve(codelets, 
+        appli_cycles=args.application_cycles,
+        min_cycles=args.min_cycles,
+        min_coverage=args.min_coverage))
+
+    for c in chosen:
+        print c.name
+
