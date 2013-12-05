3290b9b811f5263060d00d8b0e772dfdceba92bc
Chadi Akel <chadi.akel@exascale-computing.eu>
2013-12-05 13:22:01 +0100

    Solved a crash when a loop to keep does not have an invitro measure.

--- src/granularity/compute_coverage_matching.R
+++ src/granularity/compute_coverage_matching.R
@@ -5,6 +5,8 @@ suppressPackageStartupMessages(require(plyr, quietly=TRUE))
 
 options(warn=-1)
 
+TOLERANCE=0.15
+
 # load_csv: reads a csv file
 load_csv <- function(file) {
     return (tryCatch(
@@ -27,23 +29,19 @@ remove_prefix <- function(toChange, prefix) {
 }
 
 aggregateData <- function(allLoops, invitroRes) {
-    aggregatedResults = data.frame(CodeletName = allLoops$Codelet.Name)
-    aggregatedResults$invivoCallCount = allLoops$CallCount
-    aggregatedResults$invivoCPU_CLK_UNHALTED_CORE = allLoops$CPU_CLK_UNHALTED_CORE
-    aggregatedResults$invitroCallCount = invitroRes[invitroRes$Codelet.Name %in% aggregatedResults$CodeletName, ]$CallCount
-    aggregatedResults$invitroCPU_CLK_UNHALTED_CORE = invitroRes[invitroRes$Codelet.Name %in% aggregatedResults$CodeletName, ]$CPU_CLK_UNHALTED_CORE
+    aggregatedResults = merge(allLoops, invitroRes, by="Codelet.Name", suffixes=c("_invivo",
+                                                                                  "_invitro"))
     return(aggregatedResults)
 }
 
 #count the number of loops with less then 15% difference
 #between invivo and invitro sycles measure
 compute_matching <- function(aggregatedResults) {
-    res <- ddply(aggregatedResults, c("CodeletName"), function(x) {
-            data.frame(val = abs(x$invivoCPU_CLK_UNHALTED_CORE/x$invivoCallCount - x$invitroCPU_CLK_UNHALTED_CORE/x$invitroCallCount)/pmax(x$invivoCPU_CLK_UNHALTED_CORE/x$invivoCallCount, x$invitroCPU_CLK_UNHALTED_CORE/x$invitroCallCount))
+    res <- ddply(aggregatedResults, c("Codelet.Name"), function(x) {
+            data.frame(val = abs(x$CPU_CLK_UNHALTED_CORE_invivo/x$CallCount_invivo - x$CPU_CLK_UNHALTED_CORE_invitro/x$CallCount_invitro)/pmax(x$CPU_CLK_UNHALTED_CORE_invivo/x$CallCount_invivo, x$CPU_CLK_UNHALTED_CORE_invitro/x$CallCount_invitro))
     }
     )
-    return(data.frame(CodeletName = res[res$val < 0.15, ]$CodeletName))
-#~     return(nrow(res[res$val < 0.15, ])/nrow(aggregatedResults))
+    return(data.frame(CodeletName = res[res$val < TOLERANCE, ]$Codelet.Name))
 }
 
 plot_results <- function(results) {
@@ -83,17 +81,12 @@ allLoops = load_csv(paste(benchName, "/all_loops.csv", sep=""))
 allLoops = allLoops[allLoops$Codelet.Name %in% loopsToKeep$CodeletName, ] #keep only loop to keep
 allLoops = remove_prefix(allLoops, "__invivo__") #remove the prefix
 allLoops <- allLoops[sort(allLoops$Codelet.Name),]
-#~ print(allLoops)
 
 #Open invitro measures
 invitroRes = load_csv(paste(benchName, "/results/invitro_results.csv", sep=""))
 invitroRes = remove_prefix(invitroRes, "__extracted__") #remove prefix
 invitroRes = invitroRes[invitroRes$Codelet.Name %in% allLoops$Codelet.Name, ] ##keep only loop to keep
-invitroRes <- invitroRes[order(match(invitroRes[,"Codelet.Name"], allLoops[,"Codelet.Name"])), ] #order as the invivo data
-#~ print(invitroRes)
-
 aggregatedResults = aggregateData(allLoops, invitroRes) #merge invivo and invitro
-#~ print(aggregatedResults)
 
 appCycles = load_csv(paste(benchName, "/app_cycles.csv", sep=""))
 results = data.frame(BenchName = benchName)
