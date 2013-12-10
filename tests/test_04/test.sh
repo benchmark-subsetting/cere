0e3d51403bcd7ae3223270cb41e98173752e7233
Chadi Akel <chadi.akel@exascale-computing.eu>
2013-12-10 10:35:32 +0100

    We don't need to detect and include dump and rdtsc lib in bench Makefiles, as it is managed in lel.

--- tests/test_04/test.sh
+++ tests/test_04/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/loop-extractor/src/granularity/coverage.sh . "bzip2 -k -z -f dryer.jpg" make
+~/loop-extractor/src/granularity/coverage.sh . "bzip2 -k -z -f dryer.jpg" make > /dev/null 2>&1
 exit $?
