d7d12eb4b70d71201feee8891a0cd860cce4dd3c
Pablo de Oliveira <pablo@sifflez.org>
2013-12-17 18:16:24 +0100

    Fix bug with granularity solver (pulp renamed - to _ in identifiers)

--- src/granularity/granularity.py
+++ src/granularity/granularity.py
@@ -2,22 +2,23 @@
 import argparse
 import csv
 import sys
-from pulp import LpInteger, LpMinimize, LpProblem, LpStatus, LpVariable
+import os
+from pulp import LpInteger, LpMinimize, LpProblem, LpStatus, LpVariable, lpSum, GLPK
 
 class Codelet:
     def __init__(self, name, callcount, totalcycles):
         self.fullName = name
-	self.name = name.split("#")[-1]
-	self.depth = self.name.count("#")
+        self.name = name.split("#")[-1]
+        self.depth = self.name.count("#")
         self.callcount = callcount
         # we store cycles per iteration
         self.cycles = float(totalcycles)/callcount
         self.matching = True
         self.parents = set()
-	self.add_parents(self.fullName)
+        self.add_parents(self.fullName)
 
     def __repr__(self):
-        return self.name
+        return str(id(self))
 
     def add_parents(self, fullName):
 	for parent in fullName.split("#")[:-1]:
@@ -33,12 +34,14 @@ def parse(csvfile):
     for row in csvreader:
         c = Codelet(row[0], int(row[1]), int(row[2]))
         if c.name not in codelets:
-	    codelets[c.name] = c
-	else:
-	    codelets[c.name].add_parents(c.fullName)
-    
+            codelets[c.name] = c
+    else:
+        assert codelets[c.name].cycles == c.cycles
+        assert codelets[c.name].callcount == c.callcount
+        codelets[c.name].add_parents(c.fullName)
+
     for i in codelets.itervalues():
-	yield i
+        yield i
     csvfile.close()
 
 class Unsolvable(Exception):
@@ -77,24 +80,23 @@ def solve_under_coverage(codelets, appli_cycles, min_cycles=10**6,
         min_coverage=0.80):
 
     prob = LpProblem("granularity selection", LpMinimize)
-    codelet_vars = LpVariable.dicts("codelet", 
-                                    codelets, 
-                                    lowBound=0, 
-                                    upBound=1, 
-                                    cat=LpInteger) 
+    codelet_vars = LpVariable.dicts("codelet",
+                                    codelets,
+                                    lowBound=0,
+                                    upBound=1,
+                                    cat=LpInteger)
 
     # Objective function: the codelet total cost must be minimal
     # we want small codelets
-    prob += sum([codelet_vars[c]*c.cycles for c in codelets])
+    prob += lpSum([codelet_vars[c]*c.cycles for c in codelets])
 
-    # But not too small, we require at least threshold cycles for chosen 
+    # But not too small, we require at least threshold cycles for chosen
     # codelets
     for c in codelets:
-        prob += c.cycles*c.callcount >= min_cycles*codelet_vars[c]
+        prob += (min_cycles*codelet_vars[c] <= c.cycles*c.callcount)
 
     # and with good coverage
-    prob += (sum([codelet_vars[c]*c.cycles*c.callcount for c in codelets]) 
-                >= min_coverage * appli_cycles)
+    prob += (lpSum([codelet_vars[c]*c.cycles*c.callcount for c in codelets]) >= min_coverage * appli_cycles)
 
     # selected codelets should match
     for c in codelets:
@@ -102,41 +104,58 @@ def solve_under_coverage(codelets, appli_cycles, min_cycles=10**6,
             prob += codelet_vars[c] == 0
 
     # Finally we should never include both the children and the parents
-    for dad in codelets: 
+    for dad in codelets:
         for son in codelets:
             # XXX this can be implemented much more efficiently,
             # by using the transitivity of being a parent
             if not son.is_children_of(dad): continue
 
-            # We cannot select dad and son at the same time 
+            # We cannot select dad and son at the same time
             prob += codelet_vars[dad] + codelet_vars[son] <= 1
 
+    #prob.solve(GLPK())
     prob.solve()
     if (LpStatus[prob.status] != 'Optimal'):
-	raise Unsolvable()
+	    raise Unsolvable()
+
     for v in prob.variables():
+        assert v.varValue == 1.0 or v.varValue == 0.0
         if v.varValue == 1.0:
+
             for c in codelets:
-                if "codelet_"+c.name == v.name: 
+                if ("codelet_"+repr(c)) == v.name:
                     yield c
 
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find optimal codelet granularity")
-    parser.add_argument('profile_file', type=file) 
+    parser.add_argument('profile_file', type=file)
     parser.add_argument('application_cycles', type=int)
     parser.add_argument('--min_cycles', type=int, default=10**6)
     parser.add_argument('--matching', type=file, default=None)
 
     args = parser.parse_args()
 
-    codelets = list(parse(args.profile_file)) 
+    codelets = list(parse(args.profile_file))
+
+    padding = max([len(c.name) for c in codelets])
+
     for c in codelets:
-       print "{}\t\t\t\t{}".format(c.name, round(c.cycles*c.callcount/args.application_cycles*100))
-    chosen = solve(codelets, 
+       print >>sys.stderr, "# {} {}".format(c.name.ljust(padding), round((c.cycles*c.callcount)/args.application_cycles*100, 2))
+
+
+    chosen = solve(codelets,
         appli_cycles=args.application_cycles,
         min_cycles=args.min_cycles,
         matching_file=args.matching)
+
+    print >>sys.stderr, "===== Solution with coverage ====="
+
+    for c in chosen:
+       print >>sys.stderr, "> {} {}".format(c.name.ljust(padding), round((c.cycles*c.callcount)/args.application_cycles*100, 2))
+
+    print >>sys.stderr, "===== Machine readeable solution ====="
+
 
     for c in chosen:
         print c.name
