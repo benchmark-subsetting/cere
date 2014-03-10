e89e618624b67d07389b0f99afa38c3526895efc
Chadi Akel <chadi.akel@exascale-computing.eu>
2014-03-10 14:44:50 +0100

    dump.c: Temporary changes to dump scalar values.

--- src/memory_dump/dump.c
+++ src/memory_dump/dump.c
@@ -9,6 +9,10 @@
 #include <unistd.h>
 #include <string.h>
 
+//Ok global variable is bad, but we must keep
+//track of the current invocation of the loop
+static int CURR_INV=0;
+
 /* dump_region: dumps to a file "<start>.memdump"
  * the memory segment starting at address <start>
  * and ending at address <end>
@@ -95,18 +99,60 @@ void dump_mem(pid_t child, int count, void* addresses[]) {
     close(mem);
     fclose(maps);
 }
- 
+
+/*Only dumping int scalar*/
+void write_bin_file(char *path, char* name, char *type, void *adrr) {
+    char scalar_path[256];
+    char scalar_file[256];
+    if(!strcmp(type, "i32*")) {
+        snprintf(scalar_path, sizeof(scalar_path), "%s/%s", path, "integers");
+        snprintf(scalar_file, sizeof(scalar_file), "%s/%s", scalar_path, name);
+        mkdir(scalar_path, 0777);
+
+        FILE *scalar = fopen(strcat(scalar_file, ".bin"), "a");
+        if (!scalar) {
+            fprintf(stderr, "Could not create scalar file");
+            exit(-1);
+        }
+        fwrite((const void*)(adrr), sizeof(int), 1, scalar);
+        fclose(scalar);
+        //~ fprintf(stderr, "%s = %d\n", name, *(int*)(adrr));
+        //~ snprintf(tmp_value, sizeof(tmp_value), "%d", *(int*)(adrr));
+    }
+    //~ else if(!strcmp(type, "i64*")) {
+        //~ fwrite((const void*)(adrr), sizeof(float), 1, scalar);
+    //~ }
+    else if(!strcmp(type, "double*")) {
+        snprintf(scalar_path, sizeof(scalar_path), "%s/%s", path, "doubles");
+        snprintf(scalar_file, sizeof(scalar_file), "%s/%s", scalar_path, name);
+        mkdir(scalar_path, 0777);
+
+        FILE *scalar = fopen(strcat(scalar_file, ".bin"), "a");
+        if (!scalar) {
+            fprintf(stderr, "Could not create scalar file");
+            exit(-1);
+        }
+        fwrite((const void*)(adrr), sizeof(double), 1, scalar);
+        fclose(scalar);
+    }
+}
+
 /* dump: dumps memory
  * loop_name: name of dumped loop
+ * to_dump; the invocation to dump
  * count: number of arguments
  * args: the arguments to dump 
  */
-void dump(char* loop_name, int count, ...) {
+void dump(char* loop_name, int to_dump, int count, ...) {
 
     struct stat sb;
     char path[256];
     char cwd[256];
     char dump_path[] = "dump";
+
+    //If we want to dump a particular invocation
+    //~ CURR_INV++;
+    //~ if(CURR_INV != to_dump) return;
 
     /* Keep the current working directory */
     if (getcwd(cwd, sizeof(cwd)) == NULL)
@@ -124,44 +170,51 @@ void dump(char* loop_name, int count, ...) {
     }
 
     snprintf(path, sizeof(path), "%s/%s", dump_path, loop_name);
+
     /* If dump already exists for this loop_name, skip dump */ 
     if(mkdir(path, 0777) != 0) {
         //~ fprintf(stderr, "Skip dump\n");
-        return;
+        //~ return;
     }
-    
+
     void * addresses[count];
+    char *type, *name;
     va_list ap;
     int j;
     va_start(ap, count); 
-    for(j=0; j<count; j++)
-        addresses[j] = va_arg(ap, void*); 
+    for(j=0; j<count/3; j++) {
+        addresses[j] = va_arg(ap, void*);
+        type = va_arg(ap, char*); //retrieve the type
+        name = va_arg(ap, char*); //retrieve the name
+
+        write_bin_file(path, name, type, addresses[j]);
+    }
     va_end(ap);
 
-    pid_t child;
-    child = fork();
-    /*Execution should continue through the son */
-    if(child == 0) {
-        /* Trace and freeze the child process */
-        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        raise(SIGTRAP);
-        exit(0);
-    }
-    else {
-        /* Wait for the child to stop */
-        wait(NULL);
-        if(chdir(path) != 0) {
-            fprintf(stderr, "cannot enter loop dump directory");
-            exit(-1);
-        }
-        dump_mem(child, count, addresses);
-        ptrace(PTRACE_CONT, child, NULL, NULL);
-        /* Come back to original working directory */
-        if(chdir(cwd) != 0) {
-            fprintf(stderr, "cannot come back to original working directory");
-            exit(-1);
-        }
-    }
+    //~ pid_t child;
+    //~ child = fork();
+    //~ /*Execution should continue through the son */
+    //~ if(child == 0) {
+        //~ /* Trace and freeze the child process */
+        //~ ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        //~ raise(SIGTRAP);
+        //~ exit(0);
+    //~ }
+    //~ else {
+        //~ /* Wait for the child to stop */
+        //~ wait(NULL);
+        //~ if(chdir(path) != 0) {
+            //~ fprintf(stderr, "cannot enter loop dump directory");
+            //~ exit(-1);
+        //~ }
+        //~ dump_mem(child, count, addresses);
+        //~ ptrace(PTRACE_CONT, child, NULL, NULL);
+        //~ /* Come back to original working directory */
+        //~ if(chdir(cwd) != 0) {
+            //~ fprintf(stderr, "cannot come back to original working directory");
+            //~ exit(-1);
+        //~ }
+    //~ }
 }
 
 /**********************************************************************
