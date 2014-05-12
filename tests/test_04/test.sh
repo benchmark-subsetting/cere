5e2f7bf73655edf1f6a886595241fa808542c643
Pablo de Oliveira <pablo@sifflez.org>
2014-05-12 09:37:24 +0200

    Fix hardcoded path in test_04

--- tests/test_04/test.sh
+++ tests/test_04/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 make veryclean > /dev/null 2>&1
-~/loop-extractor/src/granularity/coverage.sh . "./bzip2 -k -z -f dryer.jpg" make > /dev/null 2>&1
+../../src/granularity/coverage.sh . "./bzip2 -k -z -f dryer.jpg" make > /dev/null 2>&1
 exit $?
