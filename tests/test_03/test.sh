b38b1a74f721141cc5408c22ac45761e39fed068
Pablo de Oliveira <pablo@sifflez.org>
2014-05-12 20:20:55 +0200

    Make test_03 verbose

--- tests/test_03/test.sh
+++ tests/test_03/test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 TMPDIR=`mktemp -d`
+make veryclean
+make MODE=--dump
+LD_BIND_NOW=1 ./BT
 
-make veryclean > /dev/null 2>&1
-make MODE=--dump  > /dev/null 2>&1
-LD_BIND_NOW=1 ./BT > /dev/null 2>&1
-
-make clean > /dev/null 2>&1
-make MODE=--replay=__extracted__verify_verify__265 INSTRU=--instrument > /dev/null 2>&1
-./BT > $TMPDIR/test.replay.out
+make clean
+make MODE=--replay=__extracted__verify_verify__265 INSTRU=--instrument
+./BT
 
 rm -rf "$TMPDIR"
 if [ -f rdtsc_result.csv ]; then
