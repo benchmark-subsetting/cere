d148885968d031ed22c91246a3e1aee3a20dd658
Pablo de Oliveira <pablo@sifflez.org>
2014-05-12 09:39:03 +0200

    Fix test_05

--- tests/test_05/test.sh
+++ tests/test_05/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make veryclean > /dev/null 2>&1
 make MODE=--dump  > /dev/null 2>&1
-./FT > /dev/null 2>&1
+LD_BIND_NOW=1 ./FT > /dev/null 2>&1
 
 make clean > /dev/null 2>&1
 make MODE=--replay=__extracted__auxfnct_ilog2__48 > /dev/null 2>&1
