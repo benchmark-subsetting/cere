ce43dfa71994b28c7e482e0ad795d8f08f832949
Pablo de Oliveira <pablo@sifflez.org>
2014-05-12 20:20:27 +0200

    Make test_01 verbose

--- tests/test_01/test.sh
+++ tests/test_01/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 TMPDIR=`mktemp -d`
-make veryclean > /dev/null 2>&1
-make MODE=--dump  > /dev/null 2>&1
-LD_BIND_NOW=1 ./BT > /dev/null 2>&1
+make veryclean
+make MODE=--dump
+LD_BIND_NOW=1 ./BT
 
-make clean > /dev/null 2>&1
-make MODE=--replay=__extracted__verify_verify__265 > /dev/null 2>&1
+make clean
+make MODE=--replay=__extracted__verify_verify__265
 ./BT > $TMPDIR/test.replay.out
 
 cat $TMPDIR/test.replay.out | head -n1 > $TMPDIR/test.a
