dc6e86167f7a7ba45bea0b8cb2c30994c7326451
Pablo de Oliveira <pablo@sifflez.org>
2014-05-13 17:11:02 +0200

    Improve test driver
    * Before running tests, ensure space layout randomization is active
    * When test fails cat proper log file
    
    Change-Id: I656dde6aa9142a921a1282dae2cc31d79bf3f68a

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+RANDOMIZE_SPACE=$(cat /proc/sys/kernel/randomize_va_space)
+if test "$RANDOMIZE_SPACE" = "0"; then
+    echo "layout space randomization is required during replay"
+    echo "please type echo 1 | sudo tee /proc/sys/kernel/randomize_va_space"
+    echo "before running tests"
+    exit 127
+fi
 ../autoconf/tap-driver.sh --test-name testplan.test --log-file testplan.log --trs-file testplan.trs --color-tests yes -- ./testplan.test
