aac23fa0b12c99d79e936d320303f2f5d7aeccc5
Pablo de Oliveira <pablo@sifflez.org>
2014-05-14 14:58:24 +0200

    lel: INVITRO_CALL_COUNT can be user-defined
    By calling lel with a particular definition of INVITRO_CALL_COUNT the
    user can choose the number of in-vitro repetitions. This is particularly
    useful during tests, where one iteration is enough.
    
    Change-Id: Idf2e39c6f7be4c36e8504d8d9263f2b64ef0f322

--- tests/test_02/test.sh
+++ tests/test_02/test.sh
@@ -2,12 +2,14 @@
 
 TMPDIR=`mktemp -d`
 make veryclean
-make MODE=--dump
+make -j4 MODE=--dump
 LD_BIND_NOW=1 ./BT
 
 make clean
-make MODE=--replay=__extracted__verify_verify__265
+make -j4 INVITRO_CALL_COUNT=1 MODE=--replay=__extracted__verify_verify__265
 ./BT > $TMPDIR/test.replay.out
+
+cat $TMPDIR/test.replay.out
 
 cat $TMPDIR/test.replay.out | head -n1 > $TMPDIR/test.a
 
