47cfeb86a7bdc788582f399699e4d20a7cf7983e
Chadi Akel <chadi.akel@exascale-computing.eu>
2014-05-16 16:12:38 +0200

    The command to run was not read correctly, and changing directory between multiple benches was not working.
    Change-Id: I0743ee396e8175a27939a273cdf10e4e493f5dd6

--- src/granularity/run_coverage_and_matching.sh
+++ src/granularity/run_coverage_and_matching.sh
@@ -9,10 +9,11 @@ if [ ! $# -eq 1 ]; then
 fi
 
 FILE=$1
+ORIGIN_DIR=$PWD
 
 while read benchInfo; do
     benchName=`echo $benchInfo | cut -d ' ' -f 1`
-    runCommand=`echo $benchInfo | cut -d ' ' -f 2`
+    runCommand=`echo $benchInfo | grep -Po '".*?"'`
     echo "$benchName: $runCommand"
     BENCH_DIR=$(readlink -f $benchName) # convert BENCH_DIR to absolute path
     cd $BENCH_DIR 2> /dev/null
@@ -20,7 +21,13 @@ while read benchInfo; do
         echo "Could not change directory to $BENCH_DIR"
         continue
     fi
+    cd $ORIGIN_PATH 2> /dev/null
     touch coverage_log matching_log
     $ROOT/coverage.sh $runCommand "make INVITRO_CALL_COUNT=1" > coverage_log 2>&1
     $ROOT/matching.sh loops $runCommand "make INVITRO_CALL_COUNT=1" > matching_log 2>&1
+    cd $ORIGIN_DIR 2> /dev/null
+    if [ "$?" != "0" ] ; then
+        echo "Could not come back to original directory $ORIGIN_DIR"
+        exit 1
+    fi
 done < ${FILE}
