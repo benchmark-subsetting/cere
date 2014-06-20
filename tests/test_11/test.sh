27c40d4c19815d923b6edc0a399e08acb182d3a7
Pablo de Oliveira <pablo@sifflez.org>
2014-06-20 11:45:32 +0200

    New test_11: lbm
    Add test for issue #13 (global dump)
    
    References #13
    
    Change-Id: I20e4f04bd3accc1efa9360a1a7829d5b0ca03e75

--- tests/test_11/test.sh
+++ tests/test_11/test.sh
@@ -4,22 +4,7 @@ function do_test()
 {
     make veryclean
     make -j4 MODE="dump"
-    LD_BIND_NOW=1 ./test> $TMPDIR/test.dump.out
-    cat $TMPDIR/test.dump.out
-
-    make clean
-    make -j4 MODE="replay --region=__extracted__test_check_10" F_LIB=-lc
-    ./test > $TMPDIR/test.replay.out
-    cat $TMPDIR/test.replay.out
-
-    cat $TMPDIR/test.dump.out | grep "value =" | head -n1 > $TMPDIR/test.a
-    cat $TMPDIR/test.replay.out | grep "value =" | head -n1 > $TMPDIR/test.b
-    
-    if [ ! -s $TMPDIR/test.a ]; then
-        return 1
-    fi
-    
-    diff -u $TMPDIR/test.a $TMPDIR/test.b
+    LD_BIND_NOW=1 ./lbm 20 reference.dat 0 1 100_100_130_ldc.of
 }
 
 source ../source.sh
