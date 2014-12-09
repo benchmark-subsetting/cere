555b57499d0c50c08a5a03ff641104d1d142e050
Chadi AKEL <chadi.akel@exascale-computing.eu>
2014-12-09 14:59:56 +0100

    Better detection of the executable in the command to run the application.
    Change-Id: Iffa4a7f7c51894c9b74a5fcd3d8946386ed8a93e

--- cere/cere_filter/create_graph.py
+++ cere/cere_filter/create_graph.py
@@ -15,16 +15,10 @@ def which(program):
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
+    fpath = program.split()
+    for v in fpath:
+        if is_exe(v):
+            return v
     return None
 
 def parse_line(regex_list, line):
