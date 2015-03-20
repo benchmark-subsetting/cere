273eca4b01a6ded88c0d5df1af6121f6bbf7a1cf
Chadi Akel <chadi.akel@exascale-computing.eu>
2015-03-20 16:08:40 +0100

    Added an option to allow or not global trace measures. Fixed a bug where lec would not allow trace mode while giving a loop file.
    Change-Id: I2ace1a2d2ee90fc107b7bb682becb405ce2564f6

--- cere/cere_configure/__init__.py
+++ cere/cere_configure/__init__.py
@@ -16,6 +16,7 @@ def init_module(subparsers, cere_plugins):
     configure_parser.add_argument('--build_cmd', required=True, help="Set the command to build the application")
     configure_parser.add_argument('--measures_path', default="cere_measures", help="Directory where to save CERE measures")
     configure_parser.add_argument('--dumps_path', default="cere_dumps", help="Directory where to save CERE regions dumps")
+    configure_parser.add_argument('--multiple_trace', const=True, default=False, nargs='?', help="Enable tracing multiple region at the same time (default False)")
 
 def run(args):
     global cere_config
@@ -23,6 +24,7 @@ def run(args):
     cere_config["run_cmd"] = args.run_cmd
     cere_config["cere_measures_path"] = args.measures_path
     cere_config["cere_dumps_path"] = args.dumps_path
+    cere_config["multiple_trace"] = args.multiple_trace
 
     with open("cere.json", 'w') as config_file:
         json.dump(cere_config, config_file)
