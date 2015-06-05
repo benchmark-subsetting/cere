9437299eb7665e7591a6a2ec22e360707fc5167d
Chadi Akel <chadi.akel@exascale-computing.eu>
2015-06-05 13:55:36 +0200

    Fixed a bug in io cere checker pass.
    Change-Id: Ifd49e6dcc49fa2c18e677df7080e3ec5271ef829

--- cere/cere_io_checker/__init__.py
+++ cere/cere_io_checker/__init__.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python
-# This file is part of CERE.
-#
-# Copyright (c) 2013-2015, Universite de Versailles St-Quentin-en-Yvelines
-#
-# CERE is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License,
-# or (at your option) any later version.
-#
-# Foobar is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import shutil
@@ -115,7 +99,8 @@ def find_regions_to_trace(args_region, args_regions_file, args_invocation, args_
                 # If the IO trace already exists, we can skip the test
                 if not args_force:
                     invocations = [invocation for invocation in invocations if not utils.io_trace_exists(region, invocation)]
-                regions_to_check[region] = invocations
+                if invocations:
+                    regions_to_check[region] = invocations
     # If user passed a single region
     elif args_region:
         # If the IO trace already exists, we can skip the test
