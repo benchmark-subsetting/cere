3475b399c8c213b0384732af6b6509ef41982388
Chadi Akel <chadi.akel@exascale-computing.eu>
2015-06-25 17:35:25 +0200

    Faster tests
    Change-Id: I8189726c49716176affde4fc250f55f166524f6f

--- tests/test_06/test.sh
+++ tests/test_06/test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 make veryclean
-make -j4 MODE="dump"
+make -j4 MODE="dump --region=__cere__auxfnct_ilog2__48"
 LD_BIND_NOW=1 ./FT
 
 make clean
 make MODE="replay --region=__cere__auxfnct_ilog2__48"
 ./FT | grep '128         128'
 exit $?
-
