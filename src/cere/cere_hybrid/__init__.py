bcec3625de56d281b21e3b32c4693752467ad6dc
Chadi Akel <chadi.akel@exascale-computing.eu>
2015-08-19 11:16:23 +0200

    Updated cere hybrid to use the unique regions.csv. See previous commit
    Change-Id: I2dee3102feb65982c510a621448e217a098ca1b9

--- src/cere/cere_hybrid/__init__.py
+++ src/cere/cere_hybrid/__init__.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with CERE.  If not, see <http://www.gnu.org/licenses/>.
+import os
 import argparse
 import logging
 import subprocess
@@ -30,10 +31,16 @@ def init_module(subparsers, cere_plugins):
 def run(args):
   if not cere_configure.init():
     return False
+  args.regions_file = os.path.realpath(args.regions_file)
+  if not os.path.isfile(cere_configure.cere_config["regions_infos"]):
+    logger.error("No such file: {0}".format(cere_configure.cere_config["regions_infos"]))
+    logger.error("Did you run cere regions?")
+    return False
   #New .o files created and needed by lel are stored in CERE_OBJECTS
   #and is populated by lec
   try:
-    logger.debug(subprocess.check_output("{0} MODE=\"original --hybrid --regions-file={1}\" -B".format(cere_configure.cere_config["build_cmd"], args.regions_file), stderr=subprocess.STDOUT, shell=True))
+    logger.debug(subprocess.check_output("{0} MODE=\"original --hybrid --regions-file={1} \
+    --regions-infos={2}\" -B".format(cere_configure.cere_config["build_cmd"], args.regions_file, cere_configure.cere_config["regions_infos"]), stderr=subprocess.STDOUT, shell=True))
   except subprocess.CalledProcessError as err:
     logger.critical(str(err))
     logger.critical(err.output)
