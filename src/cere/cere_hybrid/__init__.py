00e727fd5aee55fdf1d193e9365158c7326a1c52
Chadi Akel <chadi.akel@exascale-computing.eu>
2016-03-07 10:18:17 +0100

    Extraction granularity can be chosen for hybrid command
    Users can now choose to extract the loop (default) or the function
    containing the loop into separated files. Extraction at function level
    preserves the context and therefore optimisations for the concerned
    loop.
    
    Change-Id: I4ee62720efaf354e7e30b31d02aa51bdc20ee00a

--- src/cere/cere_hybrid/__init__.py
+++ src/cere/cere_hybrid/__init__.py
@@ -20,33 +20,51 @@ import argparse
 import logging
 import subprocess
 import cere_configure
+import errno
 import common.variables as var
 
 logger = logging.getLogger('Hybrid')
 
 def init_module(subparsers, cere_plugins):
   cere_plugins["hybrid"] = run
-  flag_parser = subparsers.add_parser("hybrid", help="test optimal optimization flags")
-  flag_parser.add_argument('--regions-file', required=True, help="file to compile")
-  flag_parser.add_argument('--region', help="Region to instrument")
+  hybrid_parser = subparsers.add_parser("hybrid", help="test optimal optimization flags")
+  hybrid_parser.add_argument('--regions-file', required=True, help="file to compile")
+  hybrid_parser.add_argument('--extraction-lvl', default="loop", choices=["loop","function"], help="Extract region at loop or function granularity. Default is loop")
+  hybrid_parser.add_argument('--noinstrumentation', action='store_true', help="Hybrid without instrumentation")
 
 def run(args):
   if not cere_configure.init():
     return False
+
   args.regions_file = os.path.realpath(args.regions_file)
   if not os.path.isfile(cere_configure.cere_config["regions_infos"]):
     logger.error("No such file: {0}".format(cere_configure.cere_config["regions_infos"]))
     logger.error("Did you run cere regions?")
     return False
-  region_input=""
-  if args.region:
-    region_input="--region={0}".format(args.region)
+
+  #Check for instrumentation
+  if args.noinstrumentation:
+    instru_cmd = ""
+    logger.info("Compiling hybrid binary without instrumentation")
+  else:
+    instru_cmd = "--instrument --wrapper={0}".format(var.RDTSC_WRAPPER)
+    logger.info("Compiling hybrid binary with instrumentation")
+
+  #Remove objects file
   try:
-    logger.debug(subprocess.check_output("{0} MODE=\"original --hybrid --instrument {5} --wrapper={1} --hybrid-regions={2} \
-    --regions-infos={3} --cere-objects={4}\"".format(cere_configure.cere_config["build_cmd"], var.RDTSC_WRAPPER, args.regions_file, \
-    cere_configure.cere_config["regions_infos"], os.path.realpath("__cere__objects"), region_input), stderr=subprocess.STDOUT, shell=True))
+    os.remove(os.path.realpath("__cere__objects"))
+  except OSError as e:
+    if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
+      raise #re-raise exception if a different error occured
+
+  #Compile hybrid binary
+  try:
+    logger.debug(subprocess.check_output("{0} MODE=\"original --hybrid {1} --hybrid-regions={2} \
+    --regions-infos={3} --cere-objects={4} --extraction-lvl={5}\"".format(cere_configure.cere_config["build_cmd"], instru_cmd, args.regions_file,\
+    cere_configure.cere_config["regions_infos"], os.path.realpath("__cere__objects"), args.extraction_lvl), stderr=subprocess.STDOUT, shell=True))
   except subprocess.CalledProcessError as err:
     logger.critical(str(err))
     logger.critical(err.output)
     return False
+  logger.info("Hybrid compiled successfully")
   return True
