0c5ae3d67bbb0cd0d62efb861d15c92405274768
Pablo Oliveira <pablo@sifflez.org>
2016-05-18 23:33:40 +0200

    Tests call directly the installed cere script

--- tests/test_UI_Measure/test.sh
+++ tests/test_UI_Measure/test.sh
@@ -4,8 +4,8 @@ function do_test()
 {
     cd ../test_09
     rm -rf .cere
-    ../../cere configure --build-cmd="make -j2" --clean-cmd="make clean" --run-cmd="./bwaves"
-    ../../cere instrument --region=__cere__block_solver_bi_cgstab_block__52 --invocation=6
+    cere configure --build-cmd="make -j2" --clean-cmd="make clean" --run-cmd="./bwaves"
+    cere instrument --region=__cere__block_solver_bi_cgstab_block__52 --invocation=6
 }
 
 source ../source.sh
--- tests/test_UI_Replay/test.sh
+++ tests/test_UI_Replay/test.sh
@@ -3,9 +3,9 @@
 function do_test()
 {
     rm -rf .cere/
-    ../../cere configure --build-cmd="make -j2" --clean-cmd="make clean" --run-cmd="./bwaves"
-    ../../cere capture --region=__cere__block_solver_bi_cgstab_block__52 --invocation=6
-    ../../cere replay --region=__cere__block_solver_bi_cgstab_block__52 --invocation=6
+    cere configure --build-cmd="make -j2" --clean-cmd="make clean" --run-cmd="./bwaves"
+    cere capture --region=__cere__block_solver_bi_cgstab_block__52 --invocation=6
+    cere replay --region=__cere__block_solver_bi_cgstab_block__52 --invocation=6
 }
 
 source ../source.sh
