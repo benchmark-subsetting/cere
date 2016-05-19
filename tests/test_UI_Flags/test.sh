8d44739b33bfce81cb18fa9891eacc0e6350f43f
Pablo Oliveira <pablo@sifflez.org>
2016-05-19 14:25:43 +0200

    No default file for cere flags.
    The file is dependant of the version of LLVM so it is not possible to provide a
    common default file.

--- tests/test_UI_Flags/test.sh
+++ tests/test_UI_Flags/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make clean
 rm -f .cere/replays/* cere.log realmain.c
-cere flag --region __cere__fft3d_swarztrauber__27 --flags-file small_seq_1.csv -f
+cere flag --region __cere__fft3d_swarztrauber__27 -f small_seq_1.csv
 
 cat .cere/replays/regions_flags.csv
 exit $?
