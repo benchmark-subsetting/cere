7aa32eda59456b2056dea925cd0f868d8f62b5c9
Pablo Oliveira <pablo@sifflez.org>
2017-02-23 19:06:18 +0100

    Fix test omp instrument
     * test was always returning true :(
     * it was using old deprecated -omp-num-threads options

--- tests/test_Omp_Instrument/test.sh
+++ tests/test_Omp_Instrument/test.sh
@@ -4,9 +4,18 @@ function do_test()
 {
     make veryclean
     cere configure --build-cmd="make" --run-cmd=./test --clean-cmd="make clean" --omp
-    cere instrument --regions-file=reg --omp-num-threads=4
-    cat __cere__test_fct1_25.csv __cere__test_fct1_first.csv
+    export OMP_NUM_THREADS=4
+    cere instrument --regions-file=reg
+    if [ ! -f __cere__test_fct1_25.csv ]
+    then
+       exit 1
+    fi
+    if [ ! -f __cere__test_fct1_11.csv ]
+    then
+       exit 1
+    fi
     exit 0
 }
+
 
 source ../source.sh
