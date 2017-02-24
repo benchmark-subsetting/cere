1d4e841347a128f323fb3b1542f9a50484e9fb3d
Pablo Oliveira <pablo@sifflez.org>
2017-02-24 14:48:41 +0100

    Fix fopen,fread,fwrite wrappers in tracee_hooks.c

--- src/memory_dump/tracee_hooks.c
+++ src/memory_dump/tracee_hooks.c
@@ -165,19 +165,19 @@ FILE *fopen(const char *path, const char *mode) {
     hooks_init();
   touch_string(path);
   touch_string(mode);
-  real_fopen(path, mode);
+  return real_fopen(path, mode);
 }
 
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream) {
   if (real_fread == NULL)
     hooks_init();
   touch_mem(ptr, size, nmemb);
-  real_fread(ptr, size, nmemb, stream);
+  return real_fread(ptr, size, nmemb, stream);
 }
 
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) {
   if (real_fwrite == NULL)
     hooks_init();
   touch_mem(ptr, size, nmemb);
-  real_fwrite(ptr, size, nmemb, stream);
+  return real_fwrite(ptr, size, nmemb, stream);
 }
