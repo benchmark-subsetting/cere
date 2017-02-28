c232be60bd2813bc12cbdf1bb4f7f57d8f35ea94
Pablo Oliveira <pablo@sifflez.org>
2017-02-28 16:57:04 +0100

    Desactivate --static in test_pthread for now
    Recent change in #152 breaks --static mode in OpenMP because it adds an
    OpenMP dependency to the replay library. If gcc is used to build CERE,
    then the dependency uses GOMP runtime which is a bit messy, since the
    CERE Omp codelets use llvm iomp runtime.
    
    For now I deactivate the --static mode in the test, but the OpenMP
    dependency will probably have to be removed from the replay library
    at some point.

--- tests/test_pthread/test.sh
+++ tests/test_pthread/test.sh
@@ -5,7 +5,7 @@ function do_test()
     make veryclean
     cere configure --build-cmd="make" --run-cmd=./test --clean-cmd="make clean"
     cere capture --region=__cere__test_thread_2_body_7 --invocation=1
-    cere replay --region=__cere__test_thread_2_body_7 --invocation=1 --static
+    cere replay --region=__cere__test_thread_2_body_7 --invocation=1 # --static
     if [ ! -f __cere__test_thread_2_body_7.csv ]
     then
         exit 1
